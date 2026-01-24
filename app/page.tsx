0716229ea3fb96401005b172b0f4e51c82f1f9e5
MVCorreas <correasmv@gmail.com>
2026-01-24 18:22:57 -0300

    feat: added minimal countdown timer from react countdown circle timer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+"use client"
 import Card from "./components/Card"
+import Timer from "./components/Timer"
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>TEST YOUR SKILLS</h1>
-        <Card />
+        <Timer />
       </main>
     </div>
   );
