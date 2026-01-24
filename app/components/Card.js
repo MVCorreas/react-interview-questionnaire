f02040c19c3931d0a71dd9433c1b392b54f4ad7b
MVCorreas <correasmv@gmail.com>
2026-01-24 18:15:17 -0300

    feat: added minimal questions, and card component

---
+++ app/components/Card.js
@@ -0,0 +1,18 @@
+export default function Card () {
+    return (
+        <div>
+            <h2>Prompt</h2>
+            <ul>
+                <li>
+                    answer 1
+                </li>
+                 <li>
+                    answer 2
+                </li>
+                 <li>
+                    answer 3
+                </li>
+            </ul>
+        </div>
+    )
+}
---
+++ app/lib/questions.js
@@ -0,0 +1,20 @@
+export const reactQuestions = [
+    { 
+        id: 1, 
+        prompt: "What does JSX stand for?",
+        options: ['Javascript Syntax Extension', 'JSON Xpress', 'Javascript XML'],
+        answerIndex: 2 
+    },
+    { 
+        id: 2, 
+        prompt: "What is the difference between fragment and div?",
+        options: ['Fragments use less memory', 'Divs do not create an extra node in the DOM', 'Nothing, both have same uses'],
+        answerIndex: 0 
+    },
+    { 
+        id: 2, 
+        prompt: "What is the difference between state and props?",
+        options: ['State is internal to the component, and immutable', 'Props are passed from parent to child component for component reusability', 'Props are read-only and always create prop drilling'],
+        answerIndex: 1 
+    },
+];
