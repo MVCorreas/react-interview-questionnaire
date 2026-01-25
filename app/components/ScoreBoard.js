d2554cba1dca80634468ac89a2dea87027a23b49
MVCorreas <correasmv@gmail.com>
2026-01-25 19:01:48 -0300

    feat: added score perentage below progress bar

--- app/components/ScoreBoard.js
+++ app/components/ScoreBoard.js
@@ -1,10 +1,13 @@
 export default function ScoreBoard({ score, setScore }) {
   return (
-    <div className="w-full bg-gray-200 rounded-full h-4">
+    <div className=" w-full bg-gray-200 rounded-full h-4">
       <div
         className="bg-indigo-600 h-4 rounded-full transition-all duration-500 ease-out"
         style={{ width: `${score}%` }}
       />
+      <h3 className="flex flex-col text-center text-lg font-semibold text-gray-600 dark:text-gray-200">
+        {score} %
+      </h3>
     </div>
   );
 }
